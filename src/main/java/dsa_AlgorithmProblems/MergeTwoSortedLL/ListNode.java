ade4624290495137866508c1673c1d5f33702efd
majastamenkovic <majastam1996@icloud.com>
2024-06-17 19:13:14 -0400

    Algorithm problems

---
+++ src/main/java/dsa_AlgorithmProblems/MergeTwoSortedLL/ListNode.java
@@ -0,0 +1,9 @@
+package dsa_AlgorithmProblems.MergeTwoSortedLL;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
