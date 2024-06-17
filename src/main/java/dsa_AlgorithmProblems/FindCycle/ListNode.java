754bf01805a8459212b9944b2cc70f8ee805e599
majastamenkovic <majastam1996@icloud.com>
2024-06-17 19:06:39 -0400

    Algorithm problems

---
+++ src/main/java/dsa_AlgorithmProblems/FindCycle/ListNode.java
@@ -0,0 +1,10 @@
+package dsa_AlgorithmProblems.FindCycle;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/main/java/dsa_AlgorithmProblems/PrintMiddleLL/Node.java
@@ -0,0 +1,10 @@
+package dsa_AlgorithmProblems.PrintMiddleLL;
+
+public class Node {
+    int id;
+    Node next;
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
